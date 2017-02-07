336fc47145e822c928f1e74c6e85565ab81293aa
Pablo Zaidenvoren <pabloz@lagash.com>
2017-02-07 16:10:28 -0300

    missing exports

---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./mssql/sqlServerConnection"));
+__export(require("./mysql/mySqlConnection"));
+__export(require("./pg/pgServerConnection"));
+__export(require("./dbServerConnection"));
+__export(require("./repositoryBase"));
+//# sourceMappingURL=index.js.map
---
+++ ts/index.ts
@@ -0,0 +1,5 @@
+export * from "./mssql/sqlServerConnection";
+export * from "./mysql/mySqlConnection";
+export * from "./pg/pgServerConnection";
+export * from "./dbServerConnection";
+export * from "./repositoryBase";
